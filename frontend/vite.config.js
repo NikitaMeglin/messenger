849fa1d4ed572c06a4c10dc13fd5e3eca2f5875e
NikitaMeglin <82854183+NikitaMeglin@users.noreply.github.com>
2026-06-11 15:12:41 +0300

    Fix frontend with nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,20 +5,10 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 3000,
-        allowedHosts: [
-            'messenger-frontend-m8ga.onrender.com',
-            'localhost',
-            '0.0.0.0'
-        ]
+        port: 3000
     },
     preview: {
         host: '0.0.0.0',
-        port: 3000,
-        allowedHosts: [
-            'messenger-frontend-m8ga.onrender.com',
-            'localhost',
-            '0.0.0.0'
-        ]
+        port: 3000
     }
 })
