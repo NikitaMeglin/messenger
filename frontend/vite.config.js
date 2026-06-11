fbb43b836f656ddcbccf59e4b5dce45c02660c2a
NikitaMeglin <82854183+NikitaMeglin@users.noreply.github.com>
2026-06-11 14:40:45 +0300

    Fix allowedHosts for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0',  // Слушаем все интерфейсы
+        host: '0.0.0.0',
         port: 3000,
-        strictPort: true,
-        cors: true,
-        allowedHosts: ['10.12.248.94', 'localhost', '0.0.0.0']
+        allowedHosts: [
+            'messenger-frontend-m8ga.onrender.com',
+            'localhost',
+            '0.0.0.0'
+        ]
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 3000,
+        allowedHosts: [
+            'messenger-frontend-m8ga.onrender.com',
+            'localhost',
+            '0.0.0.0'
+        ]
     }
 })
